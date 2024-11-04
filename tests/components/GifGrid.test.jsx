117f52eb73527bd9ff339a3943cef1f818ea3289
giojodev <whoisyio@gmail.com>
2024-11-03 22:18:01 -0600

    Pruebas de GifGrid.

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,5 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import {GifGrid} from '../../src/components/GifGrid'
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs')
+
 describe('Pruebas en GifGrid', () => {
     
     const  category = 'One Punch';
@@ -8,5 +12,27 @@ describe('Pruebas en GifGrid', () => {
         // screen.debug();
         expect(screen.getByText('Cargando...'));
         expect(screen.getByText(category));
+    });
+
+    test('Debe de mostrar items cuando se cargan las imagenes useFetchGifs()', () => {
+        
+        const gifs =[{
+            id:'ABC',
+            title:'Saintama',
+            url:'https:'
+        },{
+            id:'ABC',
+            title:'Saintama',
+            url:'https:'
+        }]
+        
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading:false
+        })
+       
+        render(<GifGrid category={category}/>)
+        // screen.debug();
+        expect(screen.getAllByRole('img').length).toBe(2);
     });
 });
